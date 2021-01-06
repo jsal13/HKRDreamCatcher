494e853b34b99bdf053b7c117a50c074b3877097
James Salvatore <jimmy.c.salvatore@gmail.com>
2021-01-06 17:23:05 -0600

    chore: moved dict back in, ugh;

---
+++ DreamCatcherMod/DreamCatcher/hkitemlocdatadump.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using Modding;
+using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Collections.Generic;
+using System;
+
+namespace DreamCatcher
+{
+  public class HKItemLocDataDump : Mod, ITogglableMod
+  {
+    public override int LoadPriority() => 9999;  // TODO: ???
+    private readonly WebSocketServer _wss = new WebSocketServer(10051);
+    internal static HKItemLocDataDump Instance;
+
+    /// <summary>
+    /// Creates and starts up the Websocket Server instance.
+    /// </summary>
+    public override void Initialize()
+    {
+      Instance = this;
+      Log("Initializing Dreamcatcher HKDataDump...");
+      _wss.AddWebSocketService<SocketServer>("/data", ss => {
+        ModHooks.Instance.SetPlayerBoolHook += ss.MessageBool;
+        ModHooks.Instance.SetPlayerIntHook += ss.MessageInt;
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += ss.MessageSceneLoaded;
+      });
+      _wss.Start();
+      Log("Initialized Dreamcatcher HKDataDump!");
+    }
+
+    /// <summary>
+    /// Called when the mod is disabled; stops webserver and removes socket.
+    /// </summary>
+    public void Unload()
+    {
+      _wss.Stop();
+      _wss.RemoveWebSocketService("/data");
+    }
+  }
+}
