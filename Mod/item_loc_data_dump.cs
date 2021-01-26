f62fb10694d1d0fdc1d46a7600327cf806368679
James Salvatore <jimmy.c.salvatore@gmail.com>
2021-01-26 16:56:27 -0600

    refactor: updated code as per code critique;

--- Mod/item_loc_data_dump.cs
+++ Mod/item_loc_data_dump.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
@@ -10,139 +11,114 @@ using WebSocketSharp.Server;
 
 namespace DreamCatcher
 {
-  public class HKItemLocDataDump : Mod, ITogglableMod
-  {
-    public override int LoadPriority() => 9999;
-    private readonly WebSocketServer _wss = new WebSocketServer(10051);
-    internal static HKItemLocDataDump Instance;
-
-    public override void Initialize()
+    public class HKItemLocDataDump : Mod, ITogglableMod
     {
-      Instance = this;
+        public override int LoadPriority() => 9999;
+        private readonly WebSocketServer _wss = new WebSocketServer(10051);
+        internal static HKItemLocDataDump Instance;
+        public static Dictionary<string, string> smallAreaToGeneralArea;
 
-      Log("[...] Initializing Dreamcatcher HKItemLocDataDump Socket.");
-      _wss.AddWebSocketService<SocketServer>("/data", ss =>
-      {
-        ModHooks.Instance.SavegameLoadHook += ss.OnLoadSave;
-        ModHooks.Instance.ApplicationQuitHook += ss.OnQuit;
-        ModHooks.Instance.SetPlayerBoolHook += ss.MessageBool;
-        ModHooks.Instance.SetPlayerIntHook += ss.MessageInt;
-        On.GameManager.BeginSceneTransition += ss.ManageTransitions;
-      });
+        // TODO: Put in external file.
 
-      _wss.Start();
-      Log("[OK] Initialized Dreamcatcher HKItemLocDataDump Socket.");
-    }
-    public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
-    public static string GetSpoilerLog()
-    {
-      string userDataPath = System.IO.Path.Combine(Application.persistentDataPath, "RandomizerSpoilerLog.txt");
-      return System.IO.File.ReadAllText(userDataPath);
-    }
-
-    public static string ParseSpoilerLog(string rawSpoilerText)
-    {
-
-      Dictionary<string, string> smallAreaToGeneralArea = new Dictionary<string, string>(){
-        {"Ancestral Mound", "Forgotten Crossroads"},
-        {"Beast\'s Den", "Deepnest"},
-        {"Black Egg Temple", "Forgotten Crossroads"},
-        {"Blue Lake", "Resting Grounds"},
-        {"Cast Off Shell", "Kingdom\'s Edge"},
-        {"Colosseum", "Kingdom's Edge"},
-        {"Crystallized Mound", "Crystal Peak"},
-        {"Distant Village", "Deepnest"},
-        {"Failed Tramway", "Deepnest"},
-        {"Fungal Core", "Fungal Wastes"},
-        {"Hallownest\'s Crown", "Crystal Peak"},
-        {"Iselda", "Dirtmouth"},
-        {"Isma\'s Grove", "Royal Waterways"},
-        {"Junk Pit", "Royal Waterways"},
-        {"King\'s Pass", "Dirtmouth"},
-        {"King\'s Station", "City of Tears"},
-        {"Lake of Unn", "Greenpath"},
-        {"Leg Eater", "Fungal Wastes"},
-        {"Mantis Village", "Fungal Wastes"},
-        {"Overgrown Mound", "Fog Canyon"},
-        {"Palace Grounds", "Ancient Basin"},
-        {"Pleasure House", "City of Tears"},
-        {"Queen\'s Station", "Fungal Wastes"},
-        {"Salubra", "Forgotten Crossroads"},
-        {"Sly (Key)", "Dirtmouth"},
-        {"Sly", "Dirtmouth"},
-        {"Soul Sanctum", "City of Tears"},
-        {"Spirit\'s Glade", "Resting Grounds"},
-        {"Stag Nest", "Howling Cliffs"},
-        {"Stone Sanctuary", "Greenpath"},
-        {"Teacher\'s Archives", "Fog Canyon"},
-        {"Tower of Love", "City of Tears"},
-        {"Weaver\'s Den", "Deepnest"}
-      };
-
-      // SPOILER PARSING.
-      var spoilerText = Regex.Replace(rawSpoilerText, @"\r", "", RegexOptions.Multiline); // Weird windows thing.
-      spoilerText = Regex.Match(spoilerText, @"ALL ITEMS[\s\S]*", RegexOptions.Multiline).Value;  // Only take the ALL ITEMS part and beyond.
-      spoilerText = Regex.Replace(spoilerText, @"ALL ITEMS", "", RegexOptions.Multiline); // Remove ALL ITEMS text.
-      spoilerText = Regex.Replace(spoilerText, @" ?\(\d+\) ?", "", RegexOptions.Multiline); // Remove progression values.
-      spoilerText = Regex.Replace(spoilerText, @" ?\(Key\) ?", "", RegexOptions.Multiline); // Remove "Key" text.
-      spoilerText = Regex.Replace(spoilerText, @" \[.+?\]", "", RegexOptions.Multiline); // Remove cost information.
-      spoilerText = Regex.Replace(spoilerText, @"<---at--->.*", "", RegexOptions.Multiline); // Remove "At" and everything after the at.  We know the loc already.
-      spoilerText = Regex.Replace(spoilerText, @"SETTINGS[\s\S]*", "", RegexOptions.Multiline); // Remove Settings.
-
-      // Replace the specific area with general areas above.
-      foreach (KeyValuePair<string, string> kvp in smallAreaToGeneralArea) spoilerText = Regex.Replace(spoilerText, kvp.Key, kvp.Value, RegexOptions.Multiline);
-
-      // Splitting up the spoiler so it looks like: "areaname: item1, item2, item3, ..." 
-      var spoilerArray = spoilerText
-        .Split(new[] { "\n\n" }, StringSplitOptions.None)
-        .Where(x => !String.IsNullOrEmpty(x))
-        .ToList();
-
-      // Creates the area-to-items dictionary.
-      var acc = 0;
-      Dictionary<string, List<string>> areaItemDict = new Dictionary<string, List<string>>();
-      foreach (string row in spoilerArray)
-      {
-        var area_ = row.Split('\n')[0].TrimEnd(':');
-
-        if (areaItemDict.ContainsKey(area_)) { continue; }
-        else areaItemDict[area_] = new List<string>();
-
-        // Go through every string (after the current area) and put in the items that correspond to a row for that area.
-        // Note the area rows are NOT unique (there's more than one row with the same area) as we regex'd
-        // from specific area to general area above.
-        for (var jdx = acc; jdx < spoilerArray.Count(); jdx++)
+        public HKItemLocDataDump()
         {
-          var splitstr2 = spoilerArray[jdx].Split('\n'); //.ToList();
-          if (splitstr2[0].TrimEnd(':') != area_) continue;
-
-          // It looks like [area, item1, item2, ...] so we skip 1 to exclude area:
-          foreach (string item in splitstr2.Skip(1)) areaItemDict[area_].Add(item);
+            Stream smallAreaToGeneralAreaStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("DreamCatcher.small_area_to_general_area_mapping.csv");
+            using (StreamReader inputFile = new StreamReader(smallAreaToGeneralAreaStream))
+            {
+                smallAreaToGeneralArea = inputFile.ReadToEnd()
+                    .Split(new string[] { "\r\n" }, StringSplitOptions.None)
+                    .Select(s => s.Split(',')).ToDictionary(x => x[0], x => x[1]);
+            }
         }
-        acc += 1;
-      }
 
-      // Sort the areas alphabetically.
-      SortedDictionary<string, List<string>> areaItemDictCleaned = new SortedDictionary<string, List<string>>(
-        areaItemDict.Where(x => x.Value.Count > 0).ToDictionary(x => x.Key, x => x.Value)
-      );
-      var areaItemJson = JsonConvert.SerializeObject(areaItemDictCleaned);
-      return areaItemJson;
-    }
+        public override void Initialize()
+        {
+            Instance = this;
 
-    public static string GetAndParseSpoilerLog()
-    {
-      var spoilerText = GetSpoilerLog();
-      return ParseSpoilerLog(spoilerText);
-    }
+            Log("[...] Initializing Dreamcatcher HKItemLocDataDump Socket.");
+            _wss.AddWebSocketService<SocketServer>("/data", ss =>
+            {
+                ModHooks.Instance.SavegameLoadHook += ss.OnLoadSave;
+                ModHooks.Instance.ApplicationQuitHook += ss.OnQuit;
+                ModHooks.Instance.SetPlayerBoolHook += ss.MessageBool;
+                ModHooks.Instance.SetPlayerIntHook += ss.MessageInt;
+                On.GameManager.BeginSceneTransition += ss.ManageTransitions;
+            });
 
-    /// <summary>
-    /// Called when the mod is disabled; stops webserver and removes socket.
-    /// </summary>
-    public void Unload()
-    {
-      _wss.Stop();
-      _wss.RemoveWebSocketService("/data");
+            _wss.Start();
+            Log("[OK] Initialized Dreamcatcher HKItemLocDataDump Socket.");
+        }
+        public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public static string GetSpoilerLog()
+        {
+            string userDataPath = System.IO.Path.Combine(Application.persistentDataPath, "RandomizerSpoilerLog.txt");
+            return System.IO.File.ReadAllText(userDataPath);
+        }
+
+        private static string RegexSpoilerLog(string rawSpoilerText)
+        {
+            string spoilerText = Regex.Replace(rawSpoilerText, @"\r", "", RegexOptions.Multiline); // Weird windows thing.
+            spoilerText = Regex.Match(spoilerText, @"ALL ITEMS[\s\S]*", RegexOptions.Multiline).Value;  // Only take the ALL ITEMS part and beyond.
+            spoilerText = Regex.Replace(spoilerText, @"ALL ITEMS", "", RegexOptions.Multiline); // Remove ALL ITEMS text.
+            spoilerText = Regex.Replace(spoilerText, @" ?\(\d+\) ?", "", RegexOptions.Multiline); // Remove progression values.
+            spoilerText = Regex.Replace(spoilerText, @" ?\(Key\) ?", "", RegexOptions.Multiline); // Remove "Key" text.
+            spoilerText = Regex.Replace(spoilerText, @" \[.+?\]", "", RegexOptions.Multiline); // Remove cost information.
+            spoilerText = Regex.Replace(spoilerText, @"<---at--->.*", "", RegexOptions.Multiline); // Remove "At" and everything after the at.  We know the loc already.
+            spoilerText = Regex.Replace(spoilerText, @"SETTINGS[\s\S]*", "", RegexOptions.Multiline); // Remove Settings.
+            return spoilerText;
+        }
+
+        public static string ParseRegexedSpoilerLog(string rawSpoilerText)
+        {
+            string spoilerText = RegexSpoilerLog(rawSpoilerText);
+            // Replace the specific area with general areas above.
+            foreach (KeyValuePair<string, string> kvp in smallAreaToGeneralArea) spoilerText = Regex.Replace(spoilerText, kvp.Key, kvp.Value, RegexOptions.Multiline);
+
+            // Splitting up the spoiler so it looks like: "areaname: item1, item2, item3, ..." 
+            var spoilerArray = spoilerText
+              .Split(new[] { "\n\n" }, StringSplitOptions.None)
+              .Where(x => !String.IsNullOrEmpty(x))
+              .ToList();
+
+            // Creates the area-to-items dictionary.
+            int acc = 0;
+            var areaItemDict = new Dictionary<string, List<string>>();
+            foreach (string row in spoilerArray)
+            {
+                string area_ = row.Split('\n')[0].TrimEnd(':');
+
+                if (areaItemDict.ContainsKey(area_)) { continue; }
+                else areaItemDict[area_] = new List<string>();
+
+                // Go through every string (after the current area) and put in the items that correspond to a row for that area.
+                // Note the area rows are NOT unique (there's more than one row with the same area) as we regex'd
+                // from specific area to general area above.
+                for (int jdx = acc; jdx < spoilerArray.Count(); jdx++)
+                {
+                    string[] splitstr2 = spoilerArray[jdx].Split('\n');
+                    if (splitstr2[0].TrimEnd(':') != area_) continue;
+
+                    // It looks like [area, item1, item2, ...] so we skip 1 to exclude area:
+                    foreach (string item in splitstr2.Skip(1)) areaItemDict[area_].Add(item);
+                }
+                acc += 1;
+            }
+
+            // Sort the areas alphabetically.
+            SortedDictionary<string, List<string>> areaItemDictCleaned = new SortedDictionary<string, List<string>>(
+              areaItemDict.Where(x => x.Value.Count > 0).ToDictionary(x => x.Key, x => x.Value)
+            );
+            return JsonConvert.SerializeObject(areaItemDictCleaned);
+        }
+
+        /// <summary>
+        /// Called when the mod is disabled; stops webserver and removes socket.
+        /// </summary>
+        public void Unload()
+        {
+            _wss.Stop();
+            _wss.RemoveWebSocketService("/data");
+        }
     }
-  }
 }
--- Mod/websocket.cs
+++ Mod/websocket.cs
@@ -11,146 +11,147 @@ using WebSocketSharp.Server;
 
 namespace DreamCatcher
 {
-  internal class SocketServer : WebSocketBehavior
-  {
-    private bool messagedHerrah = false;
-    private bool messagedMonomon = false;
-    private bool messagedLurien = false;
-
-    private static readonly StreamReader srRoomMapper = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DreamCatcher.room_mappings.csv"));
-    private static readonly StreamReader srEventsToIgnore = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DreamCatcher.events_to_ignore.csv"));
-    private readonly Dictionary<string, string> roomMappings;
-    private readonly List<string> ignoreEvent;
-
-    private readonly string dreamCatcherPath = Path.Combine(Application.persistentDataPath, "DreamCatcherSpoilerLog.csv");
-
-    public SocketServer()
+    internal class SocketServer : WebSocketBehavior
     {
-      IgnoreExtensions = true;
+        private bool messagedHerrah = false;
+        private bool messagedMonomon = false;
+        private bool messagedLurien = false;
 
-      // Load the roomMappings and eventsToIgnore.
-      string[] stringSeps = new string[] { "\r\n" };
-      using (srRoomMapper) this.roomMappings = srRoomMapper
-        .ReadToEnd().Split(stringSeps, StringSplitOptions.None).Select(s => s.Split(',')).ToDictionary(x => x[0], x => x[1]);
-      using (srEventsToIgnore) this.ignoreEvent = srEventsToIgnore.ReadToEnd().Split(stringSeps, StringSplitOptions.None).ToList();
-    }
+        private readonly StreamReader srRoomMapper = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DreamCatcher.room_mappings.csv"));
+        private readonly StreamReader srEventsToIgnore = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DreamCatcher.events_to_ignore.csv"));
+        private readonly Dictionary<string, string> roomMappings;
+        private readonly List<string> ignoreEvent;
 
-    private string MakeEvent(string eventName, string eventType, string eventValue, string eventLocation = "") =>
-      JsonConvert.SerializeObject(new Dictionary<string, string>() {
-        { "event_key", eventName },
-        { "event_type", eventType },
-        { "event_value", eventValue },
-        { "event_location", eventLocation }
-    });
+        private readonly string dreamCatcherPath = Path.Combine(Application.persistentDataPath, "DreamCatcherSpoilerLog.csv");
 
-    public void Broadcast(string s) => Sessions.Broadcast(s);
-    
-    protected override void OnOpen() => Send(MakeEvent("websocket", "websocket_status", "open"));
-    protected override void OnClose(CloseEventArgs e)
-    {
-      Send(MakeEvent("websocket", "websocket_status", "closed"));
-      ModHooks.Instance.SavegameLoadHook -= OnLoadSave;
-      ModHooks.Instance.SetPlayerBoolHook -= MessageBool;
-      ModHooks.Instance.SetPlayerIntHook -= MessageInt;
-      ModHooks.Instance.ApplicationQuitHook -= OnQuit;
-      On.GameManager.BeginSceneTransition -= ManageTransitions;
-      base.OnClose(e);
-    }
-    protected override void OnError(WebSocketSharp.ErrorEventArgs e) => Send(e.Message);
-    public void OnLoadSave(int _slot) => Send(MakeEvent("game_level_event", "game_level_event", "load_game"));
-    public void OnQuit() => Send(MakeEvent("game_level_event", "game_level_event", "quit_game"));
+        public SocketServer()
+        {
+            IgnoreExtensions = true;
 
-    protected override void OnMessage(WebSocketSharp.MessageEventArgs e)
-    {
-      switch (e.Data)
-      {
-        case "/get-spoiler-log":
-          Send(MakeEvent("ping_event", "spoiler", HKItemLocDataDump.GetAndParseSpoilerLog()));
-          break;
+            // Load the roomMappings and eventsToIgnore.
+            string[] stringSeps = new string[] { "\r\n" };
+            using (srRoomMapper) this.roomMappings = srRoomMapper
+              .ReadToEnd().Split(stringSeps, StringSplitOptions.None).Select(s => s.Split(',')).ToDictionary(x => x[0], x => x[1]);
+            using (srEventsToIgnore) this.ignoreEvent = srEventsToIgnore.ReadToEnd().Split(stringSeps, StringSplitOptions.None).ToList();
+        }
 
-        case "/get-scene":
-          // Uses the scenehook.
-          UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
-          break;
+        private string MakeEvent(string eventName, string eventType, string eventValue, string eventLocation = "") =>
+          JsonConvert.SerializeObject(new Dictionary<string, string>() {
+            { "event_key", eventName },
+            { "event_type", eventType },
+            { "event_value", eventValue },
+            { "event_location", eventLocation }
+        });
 
-        case "/ping-dreamers":
-          if (PlayerData.instance.monomonDefeated && !messagedMonomon) { Send(MakeEvent("ping_event", "dreamer", "Monomon")); messagedMonomon = true; }
-          if (PlayerData.instance.lurienDefeated && !messagedLurien) { Send(MakeEvent("ping_event", "dreamer", "Lurien")); messagedLurien = true; }
-          if (PlayerData.instance.hegemolDefeated && !messagedHerrah) { Send(MakeEvent("ping_event", "dreamer", "Herrah")); messagedHerrah = true; }
-          break;
+        public void Broadcast(string s) => Sessions.Broadcast(s);
 
-        case "/refresh-dc-log":
-          // Gets contents of DC log for items already got, then sends them to the js side; if user refreshes webpage, eg.
-          var lines = System.IO.File.ReadAllLines(dreamCatcherPath).Where(x => !string.IsNullOrEmpty(x)).ToArray();
-          string joinedLines = String.Join(", ", lines.ToArray());
-          Send(MakeEvent("ping_event", "found_items", $"[{joinedLines}]"));
-          break;
+        protected override void OnOpen() => Send(MakeEvent("websocket", "websocket_status", "open"));
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Send(MakeEvent("websocket", "websocket_status", "closed"));
+            ModHooks.Instance.SavegameLoadHook -= OnLoadSave;
+            ModHooks.Instance.SetPlayerBoolHook -= MessageBool;
+            ModHooks.Instance.SetPlayerIntHook -= MessageInt;
+            ModHooks.Instance.ApplicationQuitHook -= OnQuit;
+            On.GameManager.BeginSceneTransition -= ManageTransitions;
+            base.OnClose(e);
+        }
+        protected override void OnError(WebSocketSharp.ErrorEventArgs e) => Send(e.Message);
+        public void OnLoadSave(int _slot) => Send(MakeEvent("game_level_event", "game_level_event", "load_game"));
+        public void OnQuit() => Send(MakeEvent("game_level_event", "game_level_event", "quit_game"));
 
-        case "/recreate-dc-log":
-          File.Delete(this.dreamCatcherPath);
-          File.Create(this.dreamCatcherPath).Close();
-          break;
-
-        default:
-          // add-to-dc-log has a payload arg after it; not sure how else to put it in this switch statement.
-          if (e.Data.StartsWith("/add-to-dc-log"))
-            using (StreamWriter outputFile = new StreamWriter(this.dreamCatcherPath, true))
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            switch (e.Data)
             {
-              outputFile.WriteLine($"{e.Data.Remove(0, 15)}");
+                case "/get-spoiler-log":
+                    Send(MakeEvent("ping_event", "spoiler", HKItemLocDataDump.ParseRegexedSpoilerLog(HKItemLocDataDump.GetSpoilerLog())));
+                    break;
+
+                case "/get-scene":
+                    // Uses the scenehook.
+                    UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+                    break;
+
+                case "/ping-dreamers":
+                    if (PlayerData.instance.monomonDefeated && !messagedMonomon) { Send(MakeEvent("ping_event", "dreamer", "Monomon")); messagedMonomon = true; }
+                    if (PlayerData.instance.lurienDefeated && !messagedLurien) { Send(MakeEvent("ping_event", "dreamer", "Lurien")); messagedLurien = true; }
+                    if (PlayerData.instance.hegemolDefeated && !messagedHerrah) { Send(MakeEvent("ping_event", "dreamer", "Herrah")); messagedHerrah = true; }
+                    break;
+
+                case "/refresh-dc-log":
+                    // Gets contents of DC log for items already got, then sends them to the js side; if user refreshes webpage, eg.
+                    string dcLogText = String.Join(", ", (string[])File.ReadAllLines(dreamCatcherPath).Where(x => !string.IsNullOrEmpty(x)));
+                    Send(MakeEvent("ping_event", "found_items", $"[{dcLogText}]"));
+                    break;
+
+                case "/recreate-dc-log":
+                    File.Delete(this.dreamCatcherPath);
+                    File.Create(this.dreamCatcherPath).Close();
+                    break;
+
+                default:
+                    // add-to-dc-log has a payload arg after it; not sure how else to put it in this switch statement.
+                    if (e.Data.StartsWith("/add-to-dc-log"))
+                    {
+                        using (StreamWriter outputFile = new StreamWriter(this.dreamCatcherPath, true))
+                        {
+                            outputFile.WriteLine($"{e.Data.Remove(0, 15)}");
+                        }
+                    }
+                        
+                    break;
             }
-          break;
-      }
+        }
+
+        /// See: https://radiance.host/apidocs/Hooks.html for the following hook details.
+        private bool ShouldNotProcessEvent(string eventMessage)
+        {
+            bool ignoredEvent = this.ignoreEvent.Contains(eventMessage);
+            bool ignoredStartsWith = !(eventMessage.StartsWith("killed") || eventMessage.StartsWith("kills") || eventMessage.StartsWith("newData") || eventMessage.StartsWith("opened"));
+            return ignoredEvent || ignoredStartsWith;
+        }
+
+        public void MessageBool(string eventMessage, bool value)
+        {
+            try
+            {
+                if (ShouldNotProcessEvent(eventMessage)) { return; }
+
+                UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+                var lowercaseBool = value ? "true" : "false";
+                string roomName = (string)this.roomMappings[scene.name];
+                Send(MakeEvent("item", eventMessage, lowercaseBool, roomName));
+            }
+            catch
+            {
+                // TODO: Debug to see what it's catching.
+                return;
+            }
+        }
+
+        public void MessageInt(string eventMessage, int value)
+        {
+            try
+            {
+                if (ShouldNotProcessEvent(eventMessage)) { return; }
+
+                UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+                string roomName = this.roomMappings[scene.name];
+                Send(MakeEvent("item", eventMessage, value.ToString(), roomName));
+            }
+            catch
+            {
+                // TODO: Debug to see what it's catching.
+                return;
+            }
+        }
+
+        public void ManageTransitions(On.GameManager.orig_BeginSceneTransition orig, GameManager self, GameManager.SceneLoadInfo info)
+        {
+            // The only thing we need here is to see if the user is making a new game.
+            if (info.SceneName == "GG_Entrance_Cutscene") Send(MakeEvent("game_level_event", "game_level_event", "new_game"));
+            orig(self, info);
+        }
     }
-
-    /// See: https://radiance.host/apidocs/Hooks.html for the following hook details.
-    /// 
-    private bool ShouldNotProcessEvent(string eventMessage)
-    {
-      if (this.ignoreEvent.Contains(eventMessage)) { return true; }
-      if (!(eventMessage.StartsWith("killed") || eventMessage.StartsWith("kills") || eventMessage.StartsWith("newData") || eventMessage.StartsWith("opened"))) { return true; }
-      return false;
-    }
-
-    public void MessageBool(string eventMessage, bool value)
-    {
-      try
-      {
-        if (ShouldNotProcessEvent(eventMessage)) { return; }
-
-        UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
-        var lowercaseBool = value ? "true" : "false";
-        string roomName = (string)this.roomMappings[scene.name];
-        Send(MakeEvent("item", eventMessage, lowercaseBool, roomName));
-      }
-      catch
-      {
-        // TODO: Debug to see what it's catching.
-        return;
-      }
-    }
-
-    public void MessageInt(string eventMessage, int value)
-    {
-      try
-      {
-        if (ShouldNotProcessEvent(eventMessage)) { return; }
-
-        UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
-        string roomName = (string)this.roomMappings[scene.name];
-        Send(MakeEvent("item", eventMessage, value.ToString(), roomName));
-      }
-      catch
-      {
-        // TODO: Debug to see what it's catching.
-        return;
-      }
-    }
-
-    public void ManageTransitions(On.GameManager.orig_BeginSceneTransition orig, GameManager self, GameManager.SceneLoadInfo info)
-    {
-      // The only thing we need here is to see if the user is making a new game.
-      if (info.SceneName == "GG_Entrance_Cutscene") Send(MakeEvent("game_level_event", "game_level_event", "new_game"));
-      orig(self, info);
-    }
-  }
 }
